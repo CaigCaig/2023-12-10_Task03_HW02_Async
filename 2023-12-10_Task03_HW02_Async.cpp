0592abf7cd5519ec9b57585be5755043114e043c
CaigCaig <caig@inbox.ru>
2023-12-15 16:53:59 +0300

    2023-12-15 Correction

--- 2023-12-10_Task03_HW02_Async.cpp
+++ 2023-12-10_Task03_HW02_Async.cpp
@@ -11,68 +11,23 @@ using namespace std;
 using namespace std::chrono_literals;
 using namespace std::chrono;
 
-constexpr int size_v = 1000;
+constexpr int size_v = 1000000;
 
-//#define ACCUM
-
-#ifndef ACCUM
-
-template<typename It, typename T, typename F, size_t size = 25>
-F para_for_each(It begin, It end, T init, F func)
+template<typename It, typename T, typename F, size_t size = 100000>
+void para_for_each(It begin, It end, T init, F func)
 {
     size_t curr_size = std::distance(begin, end);
-    if (curr_size <= size) return for_each(begin, end, func);
+    if (curr_size <= size)
+    {
+        for_each(begin, end, func);
+        return;
+    }
     auto mid = begin;
     advance(mid, curr_size/2);
-    auto ft_res = async(para_for_each<It, T, F>, begin, mid, init, func);
-    auto l_res = para_for_each(mid, end, init, func);
-    return ft_res.get() + l_res;
+    async(para_for_each<It, T, F>, begin, mid, init, func);
+    para_for_each(mid, end, init, func);
+
 }
-
-/*
-template<typename It, typename F>
-void para_for_each(It begin, It end, F func)
-{
-    unique_lock<mutex> lg(m);
-    const auto length = std::distance(begin, end);
-    if (0 == length) return;
-    int numSegments = thread::hardware_concurrency() - 1;
-    if (numSegments == 1)
-    {
-        return for_each(func.begin(), func.end(), [&](int& i) {sum += i; });
-    }
-    int segmentSize = length / numSegments;
-    auto next = begin;
-    advance(next, segmentSize);
-
-    vector<future<void>> futures(numSegments);
-    int segment = 0;
-    lg.unlock();
-    for (auto& future : futures)
-    {
-        lg.lock();
-        future = async([=, &func]() {
-            auto segmentStart = begin + segment * segmentSize;
-            auto segmentEnd = segmentStart + segmentSize;
-
-            for (auto i = segmentStart; i != segmentEnd; ++i) func(*i);
-            });
-        ++segment;
-        lg.unlock();
-    }
-
-    for (auto& future : futures) future.wait();
-}
-*/
-
-/*
-template<typename It, typename F>
-void para_for_each(It range, F func, int numSegments)
-{
-    para_for_each(std::begin(range), std::end(range), func, numSegments);
-}
-*/
-
 
 int main()
 {
@@ -80,14 +35,8 @@ int main()
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<int> rnd(0, 100);
-    for (auto i = 0; i < size_v; ++i)
-    {
-        v[i] = rnd(rd);
-//        cout << v[i] << ' ';
-    }
+    for (auto i = 0; i < size_v; ++i) v[i] = rnd(rd);
     cout << endl;
-
-    //int cores = thread::hardware_concurrency();
 
     int sum = 0;
     auto start = chrono::high_resolution_clock::now();
@@ -105,32 +54,6 @@ int main()
 
     system("pause");
 }
-
-#else
-
-template<typename It, typename T, size_t size = 25>
-T para_accum(It begin, It end, T init)
-{
-    size_t curr_size = distance(begin, end);
-    if (curr_size <= size)
-    {
-        return accumulate(begin, end, init);
-    }
-    auto mid = begin;
-    advance(mid, curr_size / 2);
-    auto ft_res = async(para_accum<It, T>, begin, mid, init);
-    auto l_res = para_accum(mid, end, init);
-    return ft_res.get() + l_res;
-}
-
-int main()
-{
-    vector<int> v(100, 2);
-    int cores = thread::hardware_concurrency();
-    cout << "rez: " << para_accum(v.begin(), v.end(), 0) << endl;
-    system("pause");
-}
-#endif
 
 /*
 For_each
